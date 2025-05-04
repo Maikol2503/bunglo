17bb4799c2ef22373853d9ec87b3b5d90efdebe4
maikol <mgarrido2503@gmail.com>
2025-05-04 11:40:50 +0200

    add responsive file-upload-option

--- src/app/materials/materials.component.ts
+++ src/app/materials/materials.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { LocalstorageService } from '../../services/localstorage.service';
+import { LocalstorageService } from '../services/localstorage.service';
 import { RouterModule } from '@angular/router';
 
 @Component({
